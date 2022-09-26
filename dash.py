5b56521a8cf5e912c1ad45b98811ae845186f336
ellerman <106990217+ellerman4@users.noreply.github.com>
2022-09-26 17:17:27 -0500

    append vehicle data to dict

--- dash.py
+++ dash.py
@@ -7,28 +7,32 @@ eel.init('web')
 
 @eel.expose
 def vehicle_data():
+    vehicle_dict = {}
     # Try to make a connection via ELM327 OBD-II interface
     try:
         connection = obd.OBD(fast=False) # auto-connects to USB or RF port
 
-        vehicle_speed = connection.query(obd.commands.SPEED).value.to("mph") # send the command, and parse the response
-        vehicle_rpm = connection.query(obd.commands.RPM).value
-        vehicle_fuel = connection.query(obd.commands.FUEL_LEVEL).value
-
-        vehicle_codes = connection.query(obd.commands.DTC_count).value
+        vehicle_dict['vehicle_speed'] = connection.query(obd.commands.SPEED).value.to("mph") # send the command, and parse the response
+        vehicle_dict['vehicle_rpm'] = connection.query(obd.commands.RPM).value
+        vehicle_dict['vehicle_fuel'] = connection.query(obd.commands.FUEL_LEVEL).value
+        vehicle_dict['vehicle_codes'] = connection.query(obd.commands.DTC_count).value
+        vehicle_dict['vehicle_voltage'] = connection.query(obd.commands.ELM_VOLTAGE).value
+        vehicle_dict['vehicle_coolant'] = connection.query(obd.commands.COOLANT_TEMP).value
 
     # Use default values if failed
     except:
-        vehicle_speed = 0
-        vehicle_rpm = 0
-        vehicle_fuel = 0
-        vehicle_codes = 0
+        vehicle_dict['vehicle_speed'] = 0
+        vehicle_dict['vehicle_rpm'] = 800
+        vehicle_dict['vehicle_fuel'] = 100
+        vehicle_dict['vehicle_codes'] = 0
+        vehicle_dict['vehicle_voltage'] = 12.8
+        vehicle_dict['vehicle_coolant'] = 200
 
         print('Connection failed...')
 
-    return vehicle_speed, vehicle_rpm, vehicle_fuel, vehicle_codes
+    return vehicle_dict
 
 # Start Eel application on the splashscreen
 eel.start('templates/splash.html',
         size=(800, 600),
-        jinja_templates='templates')  # Start
+        jinja_templates='templates')  # Point to templates folder for Jinja templating
